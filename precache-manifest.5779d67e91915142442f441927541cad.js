3083acd44019b2b800120620f618f5688e5e6037
Yurii Holiuk <bionicuss@gmail.com>
2019-09-04 12:32:45 +0300

    Updates

--- precache-manifest.5779d67e91915142442f441927541cad.js
+++ precache-manifest.5779d67e91915142442f441927541cad.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f4c0541055d1cb80dc84d525e26cd92",
+    "revision": "483d214b4da56a68181b211238e3a08f",
     "url": "/react_static-list-of-todos/index.html"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
+    "revision": "235f657f08f2d04126d6",
     "url": "/react_static-list-of-todos/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react_static-list-of-todos/static/js/2.ec295295.chunk.js"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
-    "url": "/react_static-list-of-todos/static/js/main.1d2983b2.chunk.js"
+    "revision": "235f657f08f2d04126d6",
+    "url": "/react_static-list-of-todos/static/js/main.abafaf9a.chunk.js"
   },
   {
     "revision": "ea6f31786c2aed313835",
